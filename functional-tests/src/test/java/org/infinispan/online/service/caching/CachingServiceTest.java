6e1f6df0a1b6189093725d299ac8497d7632b2f2
Sebastian Laskawiec <slaskawi@redhat.com>
2017-10-23 14:13:55 +0200

    Wait until all Pods are available

--- functional-tests/src/test/java/org/infinispan/online/service/caching/CachingServiceTest.java
+++ functional-tests/src/test/java/org/infinispan/online/service/caching/CachingServiceTest.java
@@ -1,16 +1,20 @@
 package org.infinispan.online.service.caching;
 
+import io.fabric8.openshift.client.OpenShiftClient;
 import org.arquillian.cube.kubernetes.annotations.Named;
 import org.arquillian.cube.openshift.impl.requirement.RequiresOpenshift;
 import org.arquillian.cube.requirement.ArquillianConditionalRunner;
 import org.infinispan.online.service.endpoint.HotRodTester;
 import org.infinispan.online.service.endpoint.RESTTester;
+import org.infinispan.online.service.utils.ReadinessCheck;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(ArquillianConditionalRunner.class)
 @RequiresOpenshift
@@ -24,9 +28,17 @@ public class CachingServiceTest {
    @ArquillianResource
    URL restService;
 
+   @ArquillianResource
+   OpenShiftClient openShiftClient;
 
+   ReadinessCheck readinessCheck = new ReadinessCheck();
    HotRodTester hotRodTester = new HotRodTester();
    RESTTester restTester = new RESTTester();
+
+   @Before
+   public void before() {
+      readinessCheck.waitUntilAllPodsAreReady(openShiftClient, 30, TimeUnit.SECONDS);
+   }
 
    @Test
    public void should_default_cache_be_accessible_via_hot_rod() throws IOException {
