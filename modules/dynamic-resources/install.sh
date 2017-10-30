03d6bd89bf90635a6fa52fa9a1e14febc3b3446d
Sebastian Laskawiec <slaskawi@redhat.com>
2017-10-30 09:38:34 +0100

    ISPN-8397 Automatic heap configuration

--- modules/dynamic-resources/install.sh
+++ modules/dynamic-resources/install.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(dirname $0)
-
-# Add jboss user to root group
-usermod -g root -G jboss jboss
+ADDED_DIR=${SCRIPT_DIR}/added
 
 mkdir -p /usr/local/dynamic-resources
-cp -p $SCRIPT_DIR/dynamic_resources.sh /usr/local/dynamic-resources/
-
-chown -R jboss:root /usr/local/dynamic-resources/
-chmod -R g+rwX $dir /usr/local/dynamic-resources/
+cp -p $ADDED_DIR/dynamic_resources.sh $JBOSS_HOME/bin/launch
