158777fb99f4955b6ccea682fc7baff1b6a13aa0
rmacor <rmacor@redhat.com>
2017-11-08 14:14:36 +0100

    Add tests for caching service

--- functional-tests/src/test/java/org/infinispan/online/service/endpoint/HotRodTester.java
+++ functional-tests/src/test/java/org/infinispan/online/service/endpoint/HotRodTester.java
@@ -1,7 +1,5 @@
 package org.infinispan.online.service.endpoint;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
@@ -12,15 +10,21 @@ import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.client.hotrod.configuration.SaslQop;
 import org.infinispan.online.service.utils.TestObjectCreator;
 
+import static junit.framework.TestCase.assertNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.infinispan.online.service.utils.TestObjectCreator.generateConstBytes;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 public class HotRodTester implements EndpointTester {
 
    private final String serverName;
+   private String hotRodKey = "hotRodKey";
 
    public HotRodTester(String serverName) {
       this.serverName = serverName;
    }
 
-   @Override
    public void testBasicEndpointCapabilities(URL urlToService) {
       testBasicEndpointCapabilities(urlToService, true);
    }
@@ -45,7 +49,7 @@ public class HotRodTester implements EndpointTester {
       TestObjectCreator testObjectCreator = new TestObjectCreator();
 
       //when
-      while(System.currentTimeMillis() - endTime < 0) {
+      while (System.currentTimeMillis() - endTime < 0) {
          String key = testObjectCreator.getRandomString(1000);
          String value = testObjectCreator.getRandomString(1000);
          defaultCache.put(key, value);
@@ -57,20 +61,111 @@ public class HotRodTester implements EndpointTester {
       testBasicEndpointCapabilities(urlToService, false);
    }
 
-   private RemoteCache<String, String> getDefaultCache(URL urlToService, boolean authenticate) {
+   public RemoteCache<String, String> getDefaultCache(URL urlToService, boolean authenticate) {
+      RemoteCacheManager cachingService = getRemoteCacheManager(urlToService, authenticate);
+      return cachingService.getCache();
+   }
+
+   public RemoteCache<String, String> getNamedCache(URL urlToService, String cacheName, boolean authenticate) {
+      RemoteCacheManager cachingService = getRemoteCacheManager(urlToService, authenticate);
+      return cachingService.getCache(cacheName);
+   }
+
+   public RemoteCacheManager getRemoteCacheManager(URL urlToService, boolean authenticate) {
+      //given
       Configuration cachingServiceClientConfiguration = new ConfigurationBuilder()
          .addServer()
          .host(urlToService.getHost())
          .port(urlToService.getPort())
          .security().authentication().enabled(authenticate)
-            .username("test")
-            .password("test")
-            .realm("ApplicationRealm")
-            .saslMechanism("DIGEST-MD5")
-            .saslQop(SaslQop.AUTH)
-            .serverName(serverName)
+         .username("test")
+         .password("test")
+         .realm("ApplicationRealm")
+         .saslMechanism("DIGEST-MD5")
+         .saslQop(SaslQop.AUTH)
+         .serverName(serverName)
          .build();
-      RemoteCacheManager cachingService = new RemoteCacheManager(cachingServiceClientConfiguration);
-      return cachingService.getCache();
+
+      return new RemoteCacheManager(cachingServiceClientConfiguration);
+   }
+
+   public void putGetTest(URL hotRodService) {
+      RemoteCacheManager cachingService = getRemoteCacheManager(hotRodService, true);
+
+      stringPutGetTest(cachingService);
+      stringUpdateGetTest(cachingService);
+      stringRemoveTest(cachingService);
+
+      intPutGetTest(cachingService);
+      byteArrayPutGetTest(cachingService);
+   }
+
+   private void byteArrayPutGetTest(RemoteCacheManager cachingService) {
+      //given
+      RemoteCache<String, byte[]> byteArrayCache = cachingService.getCache();
+      byte[] byteArrayValue = generateConstBytes(4096);
+      //when
+      byteArrayCache.put(hotRodKey, byteArrayValue);
+      //then
+      assertArrayEquals(byteArrayValue, byteArrayCache.get(hotRodKey));
+   }
+
+   private void intPutGetTest(RemoteCacheManager cachingService) {
+      //given
+      RemoteCache<String, Integer> intCache = cachingService.getCache();
+      Integer intValue = 5;
+      //when
+      intCache.put(hotRodKey, intValue);
+      //then
+      assertEquals(intValue, intCache.get(hotRodKey));
+   }
+
+   private void stringRemoveTest(RemoteCacheManager cachingService) {
+      //given
+      RemoteCache<String, String> stringCache = cachingService.getCache();
+      stringCache.put(hotRodKey, "value");
+      //when
+      stringCache.remove(hotRodKey);
+      //then
+      assertNull(stringCache.get(hotRodKey));
+   }
+
+   private void stringPutGetTest(RemoteCacheManager cachingService) {
+      //given
+      RemoteCache<String, String> stringCache = cachingService.getCache();
+      //when
+      stringCache.put(hotRodKey, "value");
+      //then
+      assertEquals("value", stringCache.get(hotRodKey));
+   }
+
+   private void stringUpdateGetTest(RemoteCacheManager cachingService) {
+      //given
+      RemoteCache<String, String> stringCache = cachingService.getCache();
+      //when
+      stringCache.put(hotRodKey, "value");
+      stringCache.put(hotRodKey, "newValue");
+      //then
+      assertEquals("newValue", stringCache.get(hotRodKey));
+   }
+
+   public void evictionTest(URL hotRodService) {
+      //given
+      RemoteCacheManager cachingService = getRemoteCacheManager(hotRodService, true);
+      RemoteCache<String, byte[]> byteArrayCache = cachingService.getCache();
+      byte[] firstValue = generateConstBytes(4096);
+
+      String key = "key";
+      byteArrayCache.put(key, firstValue);
+      assertArrayEquals(firstValue, byteArrayCache.get(key));
+
+      //when the first entry is evicted, the test ends
+      long counter = 0;
+      while (byteArrayCache.get(key) != null) {
+         byte[] value = generateConstBytes(4096);
+
+         byteArrayCache.put(key + counter++, value);
+         assertArrayEquals(value, byteArrayCache.get(key));
+      }
    }
 }
--- functional-tests/src/test/java/org/infinispan/online/service/endpoint/RESTTester.java
+++ functional-tests/src/test/java/org/infinispan/online/service/endpoint/RESTTester.java
@@ -1,38 +1,48 @@
 package org.infinispan.online.service.endpoint;
 
-import org.infinispan.online.service.utils.TestObjectCreator;
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import static io.restassured.RestAssured.given;
+import io.restassured.response.Response;
+import org.infinispan.online.service.utils.TestObjectCreator;
 
 import io.restassured.specification.RequestSpecification;
 
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class RESTTester implements EndpointTester {
 
+   private String cache = "default";
+   private String value = "value";
+   private String key = "restKey";
+   private String newValue = "newValue";
+
    public void testBasicEndpointCapabilities(URL urlToService) {
-      post(urlToService.toString() + "rest/default/should_default_cache_be_accessible_via_REST", "test", 200);
+      post(urlToService.toString() + "rest/default/should_default_cache_be_accessible_via_REST", "test", 200, true);
+   }
+
+   public void testCacheAvailability(URL urlToService, String cache, boolean shouldBeAvailable) {
+      int expectedCode = shouldBeAvailable ? 200 : 404;
+      post(urlToService.toString() + "rest/" + cache + "/should_cache_be_accessible_via_REST", "test", expectedCode, true);
    }
 
    @Override
    public void testPutPerformance(URL urlToService, long timeout, TimeUnit timeUnit) {
+      //given
       long endTime = System.currentTimeMillis() + TimeUnit.MILLISECONDS.convert(timeout, timeUnit);
       TestObjectCreator testObjectCreator = new TestObjectCreator();
-      while(System.currentTimeMillis() - endTime < 0) {
+      //when
+      while (System.currentTimeMillis() - endTime < 0) {
          String key = testObjectCreator.getRandomString(1000);
          String value = testObjectCreator.getRandomString(1000);
-         post(value, urlToService.toString() + "rest/default/" + key, 200);
+         post(value, urlToService.toString() + "rest/default/" + key, 200, true);
       }
    }
 
    public void testIfEndpointIsProtected(URL urlToService) {
       post(urlToService.toString() + "rest/default/should_default_cache_be_accessible_via_REST", "test", 401, false);
-   }
-
-   private void post(String url, String body, int expectedCode) {
-      post(url, body, expectedCode, true);
    }
 
    private void post(String url, String body, int expectedCode, boolean authenticate) {
@@ -42,9 +52,97 @@ public class RESTTester implements EndpointTester {
 
       spec
          .body(body)
-      .when()
+         .when()
          .post(url)
-      .then()
+         .then()
          .statusCode(expectedCode);
+   }
+
+   public void putGetRemoveTest(URL urlToService) throws IOException {
+      String stringUrl = urlToService.toString();
+
+      putGetTest(stringUrl);
+      removeTest(stringUrl);
+   }
+
+   private void removeTest(String stringUrl) {
+      //given
+      put(stringUrl, key, value);
+      //when
+      removeMethod(stringUrl, key);
+      //then
+      getMethod(stringUrl, key, 404);
+   }
+
+   private void putGetTest(String stringUrl) {
+      //when
+      put(stringUrl, key, value);
+      //then
+      String restValue = getMethod(stringUrl, key);
+      assertEquals(value, restValue);
+
+      //when
+      put(stringUrl, key, newValue);
+      //then
+      restValue = getMethod(stringUrl, key);
+      assertEquals(newValue, restValue);
+   }
+
+   protected void put(String url, String body, int expectedCode, boolean authenticate) {
+      RequestSpecification spec = given();
+      if (authenticate)
+         spec = spec.auth().basic("test", "test");
+
+      spec.body(body).when().put(url).then().statusCode(expectedCode);
+   }
+
+   protected void delete(String url, String body, int expectedCode, boolean authenticate) {
+      RequestSpecification spec = given();
+      if (authenticate)
+         spec = spec.auth().basic("test", "test");
+
+      spec.body(body).when().delete(url).then().statusCode(expectedCode);
+   }
+
+   protected String get(String url, int expectedCode, boolean authenticate) {
+      RequestSpecification spec = given();
+      if (authenticate)
+         spec = spec.auth().basic("test", "test");
+
+      Response response = spec.when().get(url);
+      response.then().statusCode(expectedCode);
+      return response.andReturn().body().asString();
+   }
+
+   public void post(String urlServerAddress, String key, String value, int expectedCode) {
+      post(urlServerAddress.toString() + "rest/" + cache + "/" + key, value, expectedCode, true);
+   }
+
+   public void post(String urlServerAddress, String key, String value) {
+      post(urlServerAddress, key, value, 200);
+   }
+
+   public void put(String urlServerAddress, String key, String value, int expectedCode) {
+      put(urlServerAddress.toString() + "rest/" + cache + "/" + key, value, expectedCode, true);
+   }
+
+   public void put(String urlServerAddress, String key, String value) {
+      put(urlServerAddress, key, value, 200);
+   }
+
+   public String getMethod(String urlServerAddress, String key, int expectedCode) {
+      return get(urlServerAddress.toString() + "rest/" + cache + "/" + key, expectedCode, true);
+   }
+
+   public String getMethod(String urlServerAddress, String key) {
+      return getMethod(urlServerAddress, key, 200);
+   }
+
+   public void removeMethod(String urlServerAddress, String key, int expectedCode) {
+      delete(urlServerAddress.toString() + "rest/" + cache + "/" + key, "", expectedCode, true);
+   }
+
+   public void removeMethod(String urlServerAddress, String key) {
+      removeMethod(urlServerAddress, key, 200);
    }
 }
