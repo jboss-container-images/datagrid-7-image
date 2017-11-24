bcd4ff6a21920174c8ac10c16505445d22a794d1
Sebastian Laskawiec <slaskawi@redhat.com>
2017-11-24 13:52:49 +0100

    ISPN-8397 Memory tuning

--- modules/os-datagrid-performance-logging/added/adjust_performance_logging.sh
+++ modules/os-datagrid-performance-logging/added/adjust_performance_logging.sh
@@ -4,7 +4,7 @@ USE_PERFORMANCE_LOGGING=${USE_PERFORMANCE_LOGGING:-false}
 
 adjust_java_performance_logging() {
     local options="$@"
-    local additional_options="-XX:+AlwaysPreTouch -XX:+UnlockExperimentalVMOptions -XX:NativeMemoryTracking=summary -XX:+DisableExplicitGC"
+    local additional_options="-XX:+AlwaysPreTouch -XX:+UnlockExperimentalVMOptions -XX:NativeMemoryTracking=summary -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/heapdump"
     if [ ${USE_PERFORMANCE_LOGGING} == "true" ]; then
       echo "${options} ${additional_options}"
     else
