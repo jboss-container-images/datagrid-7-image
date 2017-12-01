58fed012d96a0ebddd71e9bc81175309baec1c15
Sebastian Laskawiec <slaskawi@redhat.com>
2017-12-01 12:59:20 +0100

    Additional logs for debugging

--- functional-tests/src/test/resources/destroy.sh
+++ functional-tests/src/test/resources/destroy.sh
@@ -6,6 +6,12 @@ oc get all,secrets,sa,templates,configmaps,daemonsets,clusterroles,rolebindings,
 echo "---- Docker PS ----"
 docker ps
 
+echo "---- Caching Service logs ----"
+oc logs caching-service-app-0
+
+echo "---- Shared Memory logs ----"
+oc logs shared-memory-service-app-0
+
 echo "---- Clearing up test resources ---"
 oc delete all,secrets,sa,templates,configmaps,daemonsets,clusterroles,rolebindings,serviceaccounts --selector=template=caching-service || true
 oc delete all,secrets,sa,templates,configmaps,daemonsets,clusterroles,rolebindings,serviceaccounts --selector=template=shared-memory-service || true
