5bb3ca7732fc208ca4a1d0246c76f2f067a9cf22
Sebastian Laskawiec <slaskawi@redhat.com>
2017-12-05 15:47:33 +0100

    Stabilize should_discover_new_cluster_members_when_scaling_up

--- functional-tests/src/test/java/org/infinispan/online/service/scaling/ScalingTester.java
+++ functional-tests/src/test/java/org/infinispan/online/service/scaling/ScalingTester.java
@@ -4,25 +4,31 @@ import io.fabric8.openshift.client.OpenShiftClient;
 import org.infinispan.online.service.endpoint.HotRodTester;
 import org.infinispan.online.service.utils.OpenShiftCommandlineClient;
 import org.infinispan.online.service.utils.ReadinessCheck;
+import org.infinispan.online.service.utils.Waiter;
 
 import java.net.URL;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ScalingTester {
 
    public void testFormingAClusterAfterScalingUp(String statefulSetName, URL hotRodService, OpenShiftCommandlineClient commandlineClient, ReadinessCheck readinessCheck, OpenShiftClient client, HotRodTester hotRodTester) {
+      //given
+      Waiter waiter = new Waiter();
+
       //when
       commandlineClient.scaleStatefulSet(statefulSetName, 2);
       readinessCheck.waitUntilTargetNumberOfReplicasAreReady(statefulSetName, 2, client);
 
-      int numberOfNodesInCluster = hotRodTester.getNumberOfNodesInTheCluster(hotRodService);
-
-      commandlineClient.scaleStatefulSet(statefulSetName, 1);
-      readinessCheck.waitUntilTargetNumberOfReplicasAreReady(statefulSetName, 1, client);
-
       //then
-      assertThat(numberOfNodesInCluster).isEqualTo(2);
+      //Even though the Pods are ready, there might be bad timing in the discovery protocol
+      //and Pods didn't manage to form a cluster yet.
+      //We need to wait in a loop to see it that really happened.
+      try {
+         waiter.waitFor(() -> hotRodTester.getNumberOfNodesInTheCluster(hotRodService) == 2);
+      } finally {
+         //cleanup
+         commandlineClient.scaleStatefulSet(statefulSetName, 1);
+         readinessCheck.waitUntilTargetNumberOfReplicasAreReady(statefulSetName, 1, client);
+      }
    }
 
 }
