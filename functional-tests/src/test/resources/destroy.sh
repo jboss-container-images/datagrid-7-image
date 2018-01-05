46e2cbd61112d3517edff18498464fd0b0e0d70a
Ryan Emerson <remerson@redhat.com>
2018-01-05 12:13:12 +0000

    Add additional debug info to debug.sh

--- functional-tests/src/test/resources/destroy.sh
+++ functional-tests/src/test/resources/destroy.sh
@@ -3,14 +3,23 @@
 echo "---- Printing out test resources ----"
 oc get all,secrets,sa,templates,configmaps,daemonsets,clusterroles,rolebindings,serviceaccounts
 
+echo "---- Describe Pods ----"
+oc describe pods
+
 echo "---- Docker PS ----"
 docker ps
 
 echo "---- Caching Service logs ----"
 oc logs caching-service-app-0
+oc logs caching-service-app-0 -c pem-to-keystore
 
 echo "---- Shared Memory logs ----"
 oc logs shared-memory-service-app-0
+oc logs shared-memory-service-app-0 -c pem-to-keystore
+
+echo "---- Test Runner logs ----"
+oc logs testrunner
+oc logs testrunner -c pem-to-truststore
 
 echo "---- Clearing up test resources ---"
 oc delete all,secrets,sa,templates,configmaps,daemonsets,clusterroles,rolebindings,serviceaccounts --selector=template=caching-service || true
